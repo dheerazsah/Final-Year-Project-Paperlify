cc0916ae4b88a27f37d0328b8935a45362d9ef31
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2023-09-22 15:12:09 +0545

    Implement Email and Password Validation
    Here email validation  is ensured to ensure the use of valid email formats during user registration. Additionally, password validation has been implemented, enforcing requirements for at least one uppercase letter, one symbol, and one number in user passwords.

--- Paperlify/home/views.py
+++ Paperlify/home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+import re
 
 # Create your views here.
 def signupPage(request):
@@ -12,16 +13,38 @@ def signupPage(request):
         fname = request.POST.get('fname')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        confirm_pasword = request.POST.get('confirm_pasword')
-        
-        myuser = User.objects.create_user(username, email, password)
-        myuser.fname = fname
+        confirm_password = request.POST.get('confirm_password')
 
-        myuser.save()
-        messages.success(request, 'Your account has been created successfully')
-        return redirect('login')
+         # Email Validation
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            messages.error(request, 'Invalid email format. Please provide a valid email.')
+
+        # Password Complexity Requirements
+        if not (re.search("[A-Z]", password) and re.search("[0-9]", password) and re.search("[!@#$%^&*]", password)):
+            messages.error(request, 'Password must contain at least one uppercase letter, one symbol, and one number.')
+
+        if password != confirm_password:
+            messages.error(request, 'Password did not match.')
+
+        # Name Validation (Alphabet with spaces)
+        if not all(char.isalpha() or char.isspace() for char in fname):
+            messages.error(request, 'Name must contain alphabetic characters with spaces.')
+
+        if User.objects.filter(username=username):
+            messages.error(request, 'Username already exists. Please try a new username.')
+
+        if User.objects.filter(email=email):
+            messages.error(request, 'Email already exists. Please try a new email.')
+
+        if not messages.get_messages(request):  # If there are no error messages
+            myuser = User.objects.create_user(username, email, password)
+            myuser.first_name = fname
+            myuser.save()
+            messages.success(request, 'Your account has been created successfully')
+            return redirect('login')
 
     return render(request, 'signup.html')
+
 
 def loginPage(request):
     if request.method == 'POST':
