1b302ab330ee7a4f14f91a7e32f67422475604c3
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2023-12-09 14:51:21 +0545

    Add: FileUpload model in admin.py
    In this commit, I have added the file upload model to the admin.py.

--- Paperlify/home/admin.py
+++ Paperlify/home/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import UserActivityLog
 admin.site.register(UserActivityLog)
+
+from .models import FileUpload
+admin.site.register(FileUpload)
