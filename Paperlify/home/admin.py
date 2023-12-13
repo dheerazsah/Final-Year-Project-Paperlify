86615240f3191f84b7913c57337d65cfc146ab50
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2023-12-13 17:00:07 +0545

    Add: comments to Admin
    In this commit, comments are added in admin.py.

--- Paperlify/home/admin.py
+++ Paperlify/home/admin.py
@@ -1,8 +1,12 @@
+#Importing necessary modules from Django for admin functionality
 from django.contrib import admin
 
-# Register your models here.
+#Importing models for UserActivityLog from the current app for registration in the Django admin
 from .models import UserActivityLog
+#Registering UserActivityLog model
 admin.site.register(UserActivityLog)
 
+#Importing models for FileUpload from the current app for registration in the Django admin
 from .models import FileUpload
+#Registering FileUpload model
 admin.site.register(FileUpload)
