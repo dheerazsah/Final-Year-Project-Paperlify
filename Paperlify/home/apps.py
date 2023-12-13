e92ffd6ec5b97f40930f28bf8ea2a30b0ecd899c
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2023-12-13 17:00:39 +0545

    Add: comment in Apps
    In this commit, comments are added in apps.py.

--- Paperlify/home/apps.py
+++ Paperlify/home/apps.py
@@ -1,6 +1,9 @@
+#Configuration for home app using the AppConfig class
 from django.apps import AppConfig
 
 
 class HomeConfig(AppConfig):
+    #Setting default auto-generated field for models in the app
     default_auto_field = 'django.db.models.BigAutoField'
+    #Assigning the name of the app
     name = 'home'
