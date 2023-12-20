dc4fed7bbb57626a59438adec2ee46792e2a4cb8
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2023-12-21 00:31:14 +0545

    Add: Data Ingestion
    In this commit the data ingestion is added.

--- nlp/main.py
+++ nlp/main.py
@@ -1,3 +1,15 @@
+# from secondaryTextSummarizer.logging import logger
+
+# logger.info("Welcome to our custom logging")
+from secondaryTextSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from secondaryTextSummarizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
