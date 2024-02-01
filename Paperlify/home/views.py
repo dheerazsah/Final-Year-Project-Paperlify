73335c94413c8c01a4de9f5a076c4c6028870115
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2024-02-02 00:34:42 +0545

    Add: Error Handling for Upload and Summarize
    In this commit, I have imported the ValidationError and added exception and error handling in the code.

--- Paperlify/home/views.py
+++ Paperlify/home/views.py
@@ -222,6 +222,8 @@ import docx2txt
 from PyPDF2 import PdfReader
 from django.shortcuts import render
 import requests
+from django.core.exceptions import ValidationError
+import os
 
 API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 HEADERS = {"Authorization": "Bearer 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"}
@@ -242,124 +244,183 @@ def dashboard(request):
     # context = {'documents': documents}
 
     if request.method == 'POST':
-        user = request.user
-        fileName = ''
-        fileSize = 0
-        contentType = ''
+        try:
+            user = request.user
+            fileName = ''
+            fileSize = 0
+            contentType = ''
 
-        if 'file' in request.FILES:
-            uploadfile = request.FILES['file']
-            # print(uploadfile.name)
-            # print(uploadfile.size)
-            # print(uploadfile.content_type)
+            if 'file' in request.FILES:
+                try:
+                    uploadfile = request.FILES['file']
+                    # print(uploadfile.name)
+                    # print(uploadfile.size)
+                    # print(uploadfile.content_type)
 
-            fileName = uploadfile.name
-            fileSize = uploadfile.size
-            contentType = uploadfile.content_type
-            # Save the file to the file system
-            # fs = FileSystemStorage()
-            # fs.save(uploadfile.name, uploadfile)
+                    fileName = uploadfile.name
+                    fileSize = uploadfile.size
+                    contentType = uploadfile.content_type
+                    # Save the file to the file system
+                    # fs = FileSystemStorage()
+                    # fs.save(uploadfile.name, uploadfile)
 
-            # Save upload information to the database 
-            file_info = FileUpload(
-                user= request.user,
-                doc_name=uploadfile.name,
-                doc_size=uploadfile.size,
-                doc_type=uploadfile.content_type
-            )
-            file_info.save()
-
-            request.session['file_info'] = {
-                'id': file_info.id,
-                'doc_name': file_info.doc_name,
-                'doc_size': file_info.doc_size,
-                'doc_type': file_info.doc_type,
-            }
-
-            # Log the user's activity
-            UserActivityLog.objects.create(
-                user=request.user,
-                activity='upload document',
-                ip_address=request.META.get('REMOTE_ADDR')
-            )
-
-            # Extract and display content for supported file types
-            if uploadfile.name.endswith('.txt'):
-                with uploadfile.open() as file:
-                    content = file.read().decode('utf-8')
-                    file_info.extracted_text = content  
-                    file_info.save()  
-                    #return render(request, 'dashboard.html', {'content': content})
-
-            elif uploadfile.name.endswith(('.doc', '.docx')):
-                content = docx2txt.process(uploadfile)
-                file_info.extracted_text = content  
-                file_info.save() 
-                #return render(request, 'dashboard.html', {'content': content})
-
-            elif uploadfile.name.endswith('.pdf'):
-                content = ''
-                pdf_reader = PdfReader(uploadfile)
-                for page_num in range(len(pdf_reader.pages)):
-                    page = pdf_reader.pages[page_num]
-                    content += page.extract_text()
-                file_info.extracted_text = content
-                file_info.save()  
-                #return render(request, 'dashboard.html', {'content': content})
-            
-        
-        if 'summarize' in request.POST:
-            input_text = request.POST.get('input_text', '')
-
-            # Check if input_text is empty
-            if not input_text.strip():
-                error_message = "Please upload a file or enter text before summarizing."
-                return render(request, 'dashboard.html', {'error_message': error_message})
-
-
-            # Summarize the content using the Hugging Face model
-            payload = {
-                "inputs": input_text,
-            }
-            response = requests.post(API_URL, headers=HEADERS, json=payload)
-            summary = response.json()
-
-            # Initialize file_info if not created before
-            # if not file_info:
-            #     #file_info = FileUpload(user=request.user, doc_name=fileName, doc_size=fileSize, doc_type=contentType)
-            #     file_info = FileUpload(user=request.user)
-
-            if summary and len(summary) > 0:
-                summarized_text = summary[0].get('summary_text', '')
-                # file_info = FileUpload(user=request.user)
-                # file_info.doc_name = fileName
-                # file_info.doc_size = fileSize
-                # file_info.doc_type = contentType
-                # file_info.extracted_text = input_text
-                # file_info.summarized_text = summarized_text
-                # file_info.save()
-
-                # Retrieve the existing record using the stored file_info ID
-                file_info_id = request.session.get('file_info', {}).get('id')
-                if file_info_id:
-                    file_info = FileUpload.objects.get(id=file_info_id)
-
-                    # Update the existing record with summarized text
-                    file_info.extracted_text = input_text
-                    file_info.summarized_text = summarized_text
+                    # Save upload information to the database 
+                    file_info = FileUpload(
+                        user= request.user,
+                        doc_name=uploadfile.name,
+                        doc_size=uploadfile.size,
+                        doc_type=uploadfile.content_type
+                    )
                     file_info.save()
 
-            # Log the user's activity
-            UserActivityLog.objects.create(
-                user=request.user,
-                activity='summarize',
-                ip_address=request.META.get('REMOTE_ADDR')
-            )
+                    request.session['file_info'] = {
+                        'id': file_info.id,
+                        'doc_name': file_info.doc_name,
+                        'doc_size': file_info.doc_size,
+                        'doc_type': file_info.doc_type,
+                    }
 
-    # user_id = request.user.id
-    #  # Filter documents based on the user_id
-    # documents = FileUpload.objects.filter(user_id=user_id).order_by('-upload_time')[:3]
-    # context = {'documents': documents}
+                    # Log the user's activity
+                    UserActivityLog.objects.create(
+                        user=request.user,
+                        activity='upload document',
+                        ip_address=request.META.get('REMOTE_ADDR')
+                    )
+
+                    # Extract and display content for supported file types
+                    if uploadfile.name.endswith('.txt'):
+                        try:
+                            with uploadfile.open() as file:
+                                content = file.read().decode('utf-8')
+                                if not content.strip():  # Check if the text is empty
+                                    messages.error(request, "No text found in the uploaded document.")
+                                    # Remove the file if no text is found
+                                    os.remove(uploadfile.path)
+                                    return render(request, 'dashboard.html', {'content': content, 'summary': summary})
+                                file_info.extracted_text = content  
+                                file_info.save()  
+                                #return render(request, 'dashboard.html', {'content': content})
+                        except Exception as e:
+                            error_message = "Error reading the uploaded text file."
+                            messages.error(request, error_message)
+                            print(f"Error reading text file: {str(e)}")
+
+                    elif uploadfile.name.endswith(('.doc', '.docx')):
+                        try:
+                            content = docx2txt.process(uploadfile)
+                            if not content.strip():
+                                messages.error(request, "No text found in the uploaded document.")
+                                # Remove the file if no text is found
+                                os.remove(uploadfile.path)
+                                return render(request, 'dashboard.html', {'content': content, 'summary': summary})
+                            file_info.extracted_text = content  
+                            file_info.save() 
+                            #return render(request, 'dashboard.html', {'content': content})
+                        except Exception as e:
+                            error_message = "Error processing the uploaded Word document."
+                            messages.error(request, error_message)
+                            print(f"Error processing doc/docx file: {str(e)}")
+
+                    elif uploadfile.name.endswith('.pdf'):
+                        try:
+                            content = ''
+                            pdf_reader = PdfReader(uploadfile)
+                            for page_num in range(len(pdf_reader.pages)):
+                                page = pdf_reader.pages[page_num]
+                                content += page.extract_text()
+                            if not content.strip():
+                                messages.error(request, "No text found in the uploaded document.")
+                                # Remove the file if no text is found
+                                os.remove(uploadfile.path)
+                                return render(request, 'dashboard.html', {'content': content, 'summary': summary})
+                            file_info.extracted_text = content
+                            file_info.save()  
+                            #return render(request, 'dashboard.html', {'content': content})
+                        except Exception as e:
+                            error_message = "Error reading the uploaded PDF document."
+                            messages.error(request, error_message)
+                            print(f"Error reading PDF file: {str(e)}")
+                    else:
+                        messages.error(request, "Unsupported file format")
+
+                except ValidationError as e:
+                    error_message = "Validation error during file upload."
+                    messages.error(request, error_message)
+                    print(f"Validation error: {str(e)}")
+                except Exception as e:
+                    error_message = "Unexpected error during file upload."
+                    messages.error(request, error_message)
+                    print(f"Unexpected error: {str(e)}")
+
+            if 'summarize' in request.POST:
+                try:
+                    input_text = request.POST.get('input_text', '')
+
+                    # Check if input_text is empty
+                    if not input_text.strip():
+                        error_message = "Please upload a file or enter text before summarizing."
+                        return render(request, 'dashboard.html', {'error_message': error_message})
+
+                    # Summarize the content using the Hugging Face model
+                    payload = {
+                        "inputs": input_text,
+                    }
+                    response = requests.post(API_URL, headers=HEADERS, json=payload)
+                    summary = response.json()
+
+                    # Initialize file_info if not created before
+                    # if not file_info:
+                    #     #file_info = FileUpload(user=request.user, doc_name=fileName, doc_size=fileSize, doc_type=contentType)
+                    #     file_info = FileUpload(user=request.user)
+
+                    if summary and len(summary) > 0:
+                        summarized_text = summary[0].get('summary_text', '')
+                        # file_info = FileUpload(user=request.user)
+                        # file_info.doc_name = fileName
+                        # file_info.doc_size = fileSize
+                        # file_info.doc_type = contentType
+                        # file_info.extracted_text = input_text
+                        # file_info.summarized_text = summarized_text
+                        # file_info.save()
+
+                        # Retrieve the existing record using the stored file_info ID
+                        file_info_id = request.session.get('file_info', {}).get('id')
+                        if file_info_id:
+                            file_info = FileUpload.objects.get(id=file_info_id)
+
+                            # Update the existing record with summarized text
+                            file_info.extracted_text = input_text
+                            file_info.summarized_text = summarized_text
+                            file_info.save()
+
+                        # Log the user's activity
+                        UserActivityLog.objects.create(
+                            user=request.user,
+                            activity='summarize',
+                            ip_address=request.META.get('REMOTE_ADDR')
+                        )
+                    
+                except requests.RequestException as e:
+                    error_message = "Error connecting to the summarization service. Please try again later."
+                    messages.error(request, error_message)
+                    print(f"Request error: {str(e)}")
+                except Exception as e:
+                    error_message = "Unexpected error during summarization."
+                    messages.error(request, error_message)
+                    print(f"Unexpected error: {str(e)}")
+        except ValidationError as e:
+            error_message = "Validation error in the form submission."
+            messages.error(request, error_message)
+            print(f"Validation error: {str(e)}")
+        except Exception as e:
+            error_message = "Unexpected error in the form submission."
+            messages.error(request, error_message)
+            print(f"Unexpected error: {str(e)}")
+            
+        # user_id = request.user.id
+        #  # Filter documents based on the user_id
+        # documents = FileUpload.objects.filter(user_id=user_id).order_by('-upload_time')[:3]
+        # context = {'documents': documents}
     
     #return render(request, 'dashboard.html', {'content': content, 'summary': summary, 'recent_documents': recent_documents})
     return render(request, 'dashboard.html', {'content': content, 'summary': summary})
