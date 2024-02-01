3ff2d56fb69b3f0eb6323dfb9b44d9a4f22fc3b4
Dhiraj Kumar Sah Kanu <dheerazsah31@gmail.com>
2024-02-01 21:11:00 +0545

    Add: Spinner and Page Loader
    In this commit, I have added page loader using JavaScript and Bootstrap Spinner.

--- Paperlify/static/script.js
+++ Paperlify/static/script.js
@@ -4,3 +4,8 @@ console.log("Script is running");
 //     // Redirect the user to the dashboard page
 //     window.location.href = "./dashboard";
 // }
+
+// const preloaderWrapper = document.querySelector('.preloader-wrapper');
+// window.addEventListener('load', function(){
+//     preloaderWrapper.classList.add('fade-out-animation');
+// });
